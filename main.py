e57e966521b5fcec59e895e04e1f067bfa1f79db
jrtomo <josephreneltomo@gmail.com>
2026-05-26 13:21:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from src import Binance
-from src import Bybit
+from src.Binance import Binance
+from src.Bybit import Bybit
+from src import Revolut
 from src import Common
 import asyncio
 
@@ -7,10 +8,10 @@ import asyncio
 
 def main():
         
-    ecartSeuil = float(input("Entrez l'écart de prix minimum en pourcentage (ex: 0.5 pour 0.5%) : "))
-    nbreAssets = int(input("Entrez le nombre d'actifs à analyser (ex: 50. La puissance de traitement de votre machine doit être pris en compte): "))
-    #ecartSeuil = 5
-    #nbreAssets = 50
+    #ecartSeuil = float(input("Entrez l'écart de prix minimum en pourcentage (ex: 0.5 pour 0.5%) : "))
+    #nbreAssets = int(input("Entrez le nombre d'actifs à analyser (ex: 50. La puissance de traitement de votre machine doit être pris en compte): "))
+    ecartSeuil = 3
+    #nbreAssets = 10
     allPerpetualsAssetsBinance = Binance.getAllPerpetualAssets()
     allPerpetualsAssetsBybit = Bybit.getAllAssets("linear")
 
@@ -24,7 +25,25 @@ def main():
 
 
     intersection = Common.commonAssets([normalizedPerpetualAssetsBinance, normalizedPerpetualAssetsBybit])
-    intersection = intersection[:nbreAssets]
+    intersection = intersection[:100]
+    
+    print(" ------------------------------------- Intersection ------------------------------------- ")
+    print(len(intersection))
+    
+    
+    #intersection = ['morphousdt', 'iostusdt', 'glmusdt', 'fluxusdt', 'fidausdt', 'heiusdt', 'pharosusdt', 'bomeusdt', 'naorisusdt', 'allousdt', 'moveusdt', 'ausdt', 'altusdt', 'auctionusdt', 'geniususdt', 'aliceusdt', 'eulusdt', '1000xecusdt', 'orderusdt', 'opusdt', 'deepusdt', 'rplusdt', 'melaniausdt', 'rsrusdt', 'avntusdt', 'jctusdt', 'mtlusdt', 'bsbusdt', 'eigenusdt', 'icntusdt', 'ognusdt', 'ffusdt', 'polusdt', 'avaxusdt', 'phausdt', 'plumeusdt', 'nilusdt', 'ptbusdt', 'kaiausdt', 'fogousdt', 'sapienusdt', 'berausdt', 'myxusdt', 'roninusdt', 'eduusdt', 'initusdt', 'maviausdt', 'aweusdt', 'bluaiusdt', 'icxusdt', 'partiusdt', 'algousdt', 'inxusdt', 'jasmyusdt', 'nxpcusdt', 'mirausdt', 'mmtusdt', 'arbusdt', 'bnbusdt', 'cakeusdt', 'grtusdt', 'astrusdt', 'espusdt', 'mocausdt', 'ariausdt', 'aixbtusdt', 'metusdt', 'imxusdt', 'meusdt', 'prlusdt', 'ethfiusdt', 'aevousdt', '1000flokiusdt', '2zusdt', 'memeusdt', 'asrusdt', 'injusdt', 'ensousdt', 'billusdt', 'alpineusdt', 'arkusdt', 'c98usdt', 'chipusdt', 'evaausdt', 'highusdt', 'mavusdt', '1000catusdt', 'clousdt', 'fluidusdt', 'bigtimeusdt', 'sagausdt', 'metisusdt', 'renderusdt', 'birbusdt', 'labusdt', 'mboxusdt', 'crvusdt', 'idusdt', 'mantausdt', 'pendleusdt']
+    
+    
+    """
+    drift = "driftusdt"
+    if drift in intersection:
+        print(f"{drift.upper()} est dans l'intersection", end="\n\n")
+        intersection.remove(drift)
+    """
+    
+    if not intersection:
+        print("Aucun actif en commun trouvé entre Binance et Bybit.")
+        return
     
     print(" ------------------------------------- Actifs analysés ------------------------------------- ")
     print(intersection, end="\n\n")
@@ -38,30 +57,9 @@ def main():
 
 
 
-    asyncio.run(Common.strategy(intersectionPerpetualAssetsBinanceFormat, intersectionPerpetualAssetsBybitFormat, intersection, mappingAllPerpetualsAssetsBinance, mappingAllPerpetualsAssetsBybit, asyncio.Queue(), ecartSeuil))
+    asyncio.run(Common.strategy(intersectionPerpetualAssetsBinanceFormat, intersectionPerpetualAssetsBybitFormat, intersection, mappingAllPerpetualsAssetsBinance, mappingAllPerpetualsAssetsBybit, asyncio.Queue(), ecartSeuil, 6))
     
 main()
-
-"""
-print(" ------------------------------------- Intersection ------------------------------------- ")
-print(intersection)
-
-print(" ------------------------------------- Intersection Binance ------------------------------------- ")
-print(intersectionPerpetualAssetsBinanceFormat)
-
-
-print(" ------------------------------------- Intersection Bybit ------------------------------------- ")
-print(intersectionPerpetualAssetsBybitFormat)
-
-
-print(" ------------------------------------- Mapping Binance ------------------------------------- ")
-print(mappingAllPerpetualsAssetsBinance)
-
-
-print(" ------------------------------------- Mapping Bybit ------------------------------------- ")
-print(mappingAllPerpetualsAssetsBybit)
-"""
-
 
 
 
